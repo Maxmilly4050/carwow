6140f8ce2041a7600a7865fbd99c79b554ec0854
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-16 16:35:41 +0300

    Getting static data for the blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .data.blog_posts import BLOG_POSTS
 
 def home(request):
-    return render(request, 'blogs/home.html')
+    return render(request, "blogs/home.html", {"blog_posts": BLOG_POSTS})
