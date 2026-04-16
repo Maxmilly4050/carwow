de50aafa2ba7b0543a61933d3ef68351640c95e5
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-16 22:16:30 +0300

    implementationof all posts display, view of a single post and the about page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     django.urls.path('', views.home, name='home'),
+    django.urls.path('post/<slug:slug>/', views.post, name='post'),
+    django.urls.path('posts/', views.all_posts, name='posts'),
+    django.urls.path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,15 @@ from .data.blog_posts import BLOG_POSTS
 
 def home(request):
     return render(request, "blogs/home.html", {"blog_posts": BLOG_POSTS})
+
+def all_posts(request):
+    return render(request, "blogs/all_posts.html", {"blog_posts": BLOG_POSTS})
+
+def post(request, slug):
+    blog_post = next((post for post in BLOG_POSTS if post["slug"] == slug), None)
+    if blog_post is None:
+        return render(request, "blogs/post_not_found.html", status=404)
+    return render(request, "blogs/post.html", {"post": blog_post})
+
+def about(request):
+    return render(request, "blogs/about.html")
