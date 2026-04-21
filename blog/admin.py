b208233ca82f8d38990d6b196c7dd47e73882b53
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-21 09:51:48 +0300

    Add Author model and update Post model with ForeignKey relationship; enhance admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_at", "category", "is_featured")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.site_header = "Carwow Blog Admin"
+admin.site.site_title = "Carwow Blog Admin Portal"
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     excerpt = models.TextField(max_length=300)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
     published_at = models.DateField()
     content = models.TextField()
     category = models.CharField(max_length=50)
@@ -13,3 +13,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
