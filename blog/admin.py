e4206d4008666dca8881ad40133f6b81ea409a1a
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-21 09:52:56 +0300

    Add Author model registration to admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Author, Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "published_at", "category", "is_featured")
     prepopulated_fields = {"slug": ("title",)}
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
-admin.site.register(Post, PostAdmin)
+
+
 admin.site.site_header = "Carwow Blog Admin"
 admin.site.site_title = "Carwow Blog Admin Portal"
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
